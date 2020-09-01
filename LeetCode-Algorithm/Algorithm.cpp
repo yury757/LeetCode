35eaff399f839a3a04254ff3db379756413934a3
yury757 <zouyuhust@outlook.com>
2020-09-01 22:56:36 +0800

    2020-09-01打卡

--- LeetCode-Algorithm/Algorithm.cpp
+++ LeetCode-Algorithm/Algorithm.cpp
@@ -77,6 +77,11 @@ int main()
     bool canVisitAllRooms(vector<vector<int>> & rooms);
     //vector<vector<int>> rooms = { {2, 3}, { }, { 2 }, {1, 3, 1} };
     //cout << canVisitAllRooms(rooms);
+
+    bool dfs486(vector<int> nums, char person, int& a, int& b);
+    bool PredictTheWinner(vector<int> & nums);
+    vector<int> nums486 = { 1, 5, 233, 7 };
+    cout << PredictTheWinner(nums486);
 }
 
 void printVector(vector<string> v) {
@@ -799,4 +804,79 @@ bool dfs841_2(int start, vector<vector<int>>& rooms, vector<int>& inrooms) {
         rooms[start][i] = start_temp;
     }
     return 0;
+}
+
+bool dfs486(vector<int> nums, char person, int& a, int& b) {
+    // 执行用时：724 ms , 在所有 C++ 提交中击败了 5.45 % 的用户
+    // 内存消耗：204.6 MB , 在所有 C++ 提交中击败了 5.08 % 的用户
+    // 请参考官方算法
+    if (nums.size() == 0) {
+        return a >= b ? 1 : 0;
+    }
+    int get_num;
+    get_num = nums[0];
+    vector<int> temp486;
+    for (int i = 1; i < nums.size(); i++) {
+        temp486.push_back(nums[i]);
+    }
+    if (person == 'a') {
+        a += get_num;
+        if (dfs486(temp486, 'b', a, b) == 1) {
+            a -= get_num;
+            return 1;
+        }
+        a -= get_num;
+    }
+    else {
+        b += get_num;
+        if (dfs486(temp486, 'a', a, b) == 0) {
+            b -= get_num;
+            return 0;
+        }
+        b -= get_num;
+    }
+    get_num = nums[nums.size() - 1];
+    nums.pop_back();
+    if (person == 'a') {
+        a += get_num;
+        if (dfs486(nums, 'b', a, b) == 1) {
+            a -= get_num;
+            return 1;
+        }
+        a -= get_num;
+        return 0;
+    }
+    else {
+        b += get_num;
+        if (dfs486(nums, 'a', a, b) == 0) {
+            b -= get_num;
+            return 0;
+        }
+        b -= get_num;
+        return 1;
+    }
+}
+
+//486. 预测赢家
+//给定一个表示分数的非负整数数组。 玩家 1 从数组任意一端拿取一个分数，随后玩家 2 继续从剩余数组任意一端拿取分数，然后玩家 1 拿，…… 。每次一个玩家只能拿取一个分数，分数被拿取之后不再可取。直到没有剩余分数可取时游戏结束。最终获得分数总和最多的玩家获胜。
+//给定一个表示分数的数组，预测玩家1是否会成为赢家。你可以假设每个玩家的玩法都会使他的分数最大化。
+//示例 1：
+//输入：[1, 5, 2]
+//输出：False
+//解释：一开始，玩家1可以从1和2中进行选择。
+//如果他选择 2（或者 1 ），那么玩家 2 可以从 1（或者 2 ）和 5 中进行选择。如果玩家 2 选择了 5 ，那么玩家 1 则只剩下 1（或者 2 ）可选。
+//所以，玩家 1 的最终分数为 1 + 2 = 3，而玩家 2 为 5 。
+//因此，玩家 1 永远不会成为赢家，返回 False 。
+//示例 2：
+//输入：[1, 5, 233, 7]
+//输出：True
+//解释：玩家 1 一开始选择 1 。然后玩家 2 必须从 5 和 7 中进行选择。无论玩家 2 选择了哪个，玩家 1 都可以选择 233 。
+//最终，玩家 1（234 分）比玩家 2（12 分）获得更多的分数，所以返回 True，表示玩家 1 可以成为赢家。
+//提示：
+//1 <= 给定的数组长度 <= 20.
+//数组里所有分数都为非负数且不会大于 10000000 。
+//如果最终两个玩家的分数相等，那么玩家 1 仍为赢家。
+bool PredictTheWinner(vector<int>& nums) {
+    int a = 0, b = 0;
+    return dfs486(nums, 'a', a, b);
 }
