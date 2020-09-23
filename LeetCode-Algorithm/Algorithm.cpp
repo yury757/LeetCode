0b7dae183088b0bbe12b99ab6afc36ac185b21b0
yury757 <zouyuhust@outlook.com>
2020-09-23 22:05:02 +0800

    2020-09-23打卡

--- LeetCode-Algorithm/Algorithm.cpp
+++ LeetCode-Algorithm/Algorithm.cpp
@@ -1798,3 +1798,46 @@ TreeNode* convertBST(TreeNode* root) {
     return root;
 }
 
+//617. 合并二叉树
+//给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+//你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+//示例 1:
+//输入:
+//Tree 1                     Tree 2
+//    1                         2
+//   / \                       / \
+//  3   2                     1   3
+// /                           \   \
+//5                             4   7
+//输出:
+//合并后的树:
+//    3
+//   / \
+//  4   5
+// / \   \
+//5   4   7
+void dfs617(TreeNode* &t1, TreeNode* &t2) {
+    if (t1 == nullptr && t2 == nullptr) {
+        return;
+    }
+    if (t1 == nullptr) {
+        t1 = t2;
+        return;
+    }
+    else {
+        if (t2 == nullptr) {
+            t2 = new TreeNode(0);
+        }
+        else {
+            t1->val = t1->val + t2->val;
+        }
+    }
+    dfs617(t1->left, t2->left);
+    dfs617(t1->right, t2->right);
+}
+
+TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+    dfs617(t1, t2);
+    return t1;
+}
+
